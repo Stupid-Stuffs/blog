9e2f4802fe4df574c7835d9c6e3d313be0ad041b
Le Viet-Hoang <hashdotlee@gmail.com>
2023-07-20 16:24:06 +0700

    update

--- pages/user/write.js
+++ pages/user/write.js
@@ -8,145 +8,116 @@ import { getTokenFromLocalStorage } from '@/lib/token'
 import FileUploader from '@/components/UploadFile'
 
 const MDEditor = dynamic(() => import('@uiw/react-md-editor').then((mod) => mod.default), {
-    ssr: false,
+  ssr: false,
 })
 
 export default function WriteBlog() {
-    const [value, setValue] = useState('**Hello world!!!**')
-    const [slug, setSlug] = useState('')
-    const [title, setTitle] = useState('')
-    const [summary, setSummary] = useState('')
-    const [tags, setTags] = useState('')
-    const [image, setImage] = useState('')
-    const [audio, setAudio] = useState('')
-    const [video, setVideo] = useState('')
+  const [value, setValue] = useState('**Hello world!!!**')
+  const [slug, setSlug] = useState('')
+  const [title, setTitle] = useState('')
+  const [summary, setSummary] = useState('')
+  const [tags, setTags] = useState('')
+  const [image, setImage] = useState('')
 
-    const router = useRouter()
+  const router = useRouter()
 
-    const onPublish = () => {
-        let content = value
-        if (image)
-            content += `
+  const onPublish = () => {
+    let content = value
+    if (image)
+      content += `
         
            ![${image}](${image}) 
 
         `
-        if (audio)
-            content += `
-
-            <audio controls="controls">
-            <source type="audio/mp3" src="${audio}"></source>
-            <p>Your browser does not support the audio element.</p>
-            </audio>
-
-        `
-        if (video)
-            content += `
-
-            <video controls>
-            <source type="video/mp4" src="${video}"></source>
-            <source type="video/webm" src="${video}"></source>
-            <p>Your browser does not support the video element.</p>
-            </video>
-
-        `
-        api
-            .post(
-                '/api/blog',
-                {
-                    slug,
-                    title,
-                    summary,
-                    tags: tags.split(',').map((item) => ({ name: item })),
-                    content: content,
-                },
-                {
-                    headers: {
-                        Authorization: `Bearer ${getTokenFromLocalStorage()}`,
-                    },
-                }
-            )
-            .then(() => {
-                toast.success('Success! You can view your blog now')
-                router.push('/')
-            })
-            .catch((e) => toast.error(String(e)))
-    }
-    return (
-        <MainLayout>
-            <div data-color-mode="dark">
-                <div className="flex justify-between border-t pt-6">
-                    <div className="text-xl font-semibold">✍️ Write new blog</div>
-                    <button
-                        onClick={() => onPublish()}
-                        className="rounded-md border bg-primary-200 px-4 py-2 text-sm shadow-sm"
-                    >
-                        Publish your nice blog 🚀
-                    </button>
-                </div>
-                <div>
-                    <div className="my-2 flex w-full flex-col gap-2">
-                        <label htmlFor="title">Title*</label>
-                        <input
-                            id={'title'}
-                            className="rounded-md border border-gray-300 p-2"
-                            value={title}
-                            onChange={(e) => {
-                                setTitle(e.currentTarget.value)
-                            }}
-                        />
-                    </div>
-                    <div className="my-2 flex w-full flex-col gap-2">
-                        <label htmlFor="slug">Slug* (shoule be unique)</label>
-                        <input
-                            id={'slug'}
-                            className="rounded-md border border-gray-300 p-2"
-                            value={slug}
-                            onChange={(e) => {
-                                setSlug(e.currentTarget.value)
-                            }}
-                        />
-                    </div>
-                    <div className="my-2 flex w-full flex-col gap-2">
-                        <label htmlFor="summary">Summary*</label>
-                        <textarea
-                            id="summay"
-                            value={summary}
-                            className="rounded-md border border-gray-300 p-2"
-                            onChange={(e) => {
-                                setSummary(e.currentTarget.value)
-                            }}
-                        />
-                    </div>
-                    <div className="my-2 flex w-full flex-col gap-2">
-                        <label htmlFor="tags">Tags* (separate by comma ,)</label>
-                        <input
-                            id="tags"
-                            value={tags}
-                            className="rounded-md border border-gray-300 p-2"
-                            onChange={(e) => {
-                                setTags(e.currentTarget.value)
-                            }}
-                        />
-                    </div>
-                    <div className="my-2 flex w-full flex-col gap-2">
-                        <label>Upload image</label>
-                        <FileUploader onFinish={(link) => setImage(link)} />
-                    </div>
-                    <div className="my-2 flex w-full flex-col gap-2">
-                        <label>Upload audio</label>
-                        <FileUploader onFinish={(link) => setAudio(link)} />
-                    </div>
-                    <div className="my-2 flex w-full flex-col gap-2">
-                        <label>Upload video</label>
-                        <FileUploader onFinish={(link) => setVideo(link)} />
-                    </div>
-                </div>
-                <div>
-                    <label>Content*</label>
-                    <MDEditor className="mt-3" value={value} onChange={setValue} />
-                </div>
-            </div>
-        </MainLayout>
-    )
+    api
+      .post(
+        '/api/blog',
+        {
+          slug,
+          title,
+          summary,
+          tags: tags.split(',').map((item) => ({ name: item })),
+          content: content,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${getTokenFromLocalStorage()}`,
+          },
+        }
+      )
+      .then(() => {
+        toast.success('Success! You can view your blog now')
+        router.push('/')
+      })
+      .catch((e) => toast.error(String(e)))
+  }
+  return (
+    <MainLayout>
+      <div data-color-mode="dark">
+        <div className="flex justify-between border-t pt-6">
+          <div className="text-xl font-semibold">✍️ Write new blog</div>
+          <button
+            onClick={() => onPublish()}
+            className="rounded-md border bg-primary-200 px-4 py-2 text-sm shadow-sm"
+          >
+            Publish your nice blog 🚀
+          </button>
+        </div>
+        <div>
+          <div className="my-2 flex w-full flex-col gap-2">
+            <label htmlFor="title">Title*</label>
+            <input
+              id={'title'}
+              className="rounded-md border border-gray-300 p-2"
+              value={title}
+              onChange={(e) => {
+                setTitle(e.currentTarget.value)
+              }}
+            />
+          </div>
+          <div className="my-2 flex w-full flex-col gap-2">
+            <label htmlFor="slug">Slug* (shoule be unique)</label>
+            <input
+              id={'slug'}
+              className="rounded-md border border-gray-300 p-2"
+              value={slug}
+              onChange={(e) => {
+                setSlug(e.currentTarget.value)
+              }}
+            />
+          </div>
+          <div className="my-2 flex w-full flex-col gap-2">
+            <label htmlFor="summary">Summary*</label>
+            <textarea
+              id="summay"
+              value={summary}
+              className="rounded-md border border-gray-300 p-2"
+              onChange={(e) => {
+                setSummary(e.currentTarget.value)
+              }}
+            />
+          </div>
+          <div className="my-2 flex w-full flex-col gap-2">
+            <label htmlFor="tags">Tags* (separate by comma ,)</label>
+            <input
+              id="tags"
+              value={tags}
+              className="rounded-md border border-gray-300 p-2"
+              onChange={(e) => {
+                setTags(e.currentTarget.value)
+              }}
+            />
+          </div>
+          <div className="my-2 flex w-full flex-col gap-2">
+            <label>Upload image</label>
+            <FileUploader onFinish={(link) => setImage(link)} />
+          </div>
+        </div>
+        <div>
+          <label>Content*</label>
+          <MDEditor className="mt-3" value={value} onChange={setValue} />
+        </div>
+      </div>
+    </MainLayout>
+  )
 }
