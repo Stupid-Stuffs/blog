8a0f14a4b7d54dd9b8866f55b2cccba377853723
Le Viet-Hoang <hashdotlee@gmail.com>
2023-07-20 08:48:42 +0700

    fix bugs

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -6,17 +6,21 @@ import CustomLink from './Link'
 import TOCInline from './TOCInline'
 import Pre from './Pre'
 import { BlogNewsletterForm } from './NewsletterForm'
+import dynamic from 'next/dynamic'
+
+const PostLayout = dynamic(() => import('@/layouts/PostLayout'), { ssr: false })
 
 export const MDXComponents = {
-  Image,
-  TOCInline,
-  a: CustomLink,
-  pre: Pre,
-  BlogNewsletterForm: BlogNewsletterForm,
-  wrapper: ({ components, layout, ...rest }) => {
-    const Layout = require(`../layouts/${layout}`).default
-    return <Layout {...rest} />
-  },
+    Image,
+    TOCInline,
+    a: CustomLink,
+    pre: Pre,
+    BlogNewsletterForm: BlogNewsletterForm,
+    wrapper: ({ components, layout, ...rest }) => {
+        const Layout = require(`../layouts/${layout}`).default
+        if (layout === 'PostLayout') Layout = PostLayout;
+        return <Layout {...rest} />
+    },
 }
 
 export const MDXLayoutRenderer = ({ layout, mdxSource, ...rest }) => {
